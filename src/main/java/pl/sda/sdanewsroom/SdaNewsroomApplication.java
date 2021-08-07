42170b4d10bba2ea1bc28b1e1e4f196633baa586
artur <artur@zochniak.pl>
2021-08-08 00:58:11 +0200

    chore: spring initializr

---
+++ src/main/java/pl/sda/sdanewsroom/SdaNewsroomApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.sdanewsroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaNewsroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdaNewsroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sda/sdanewsroom/SdaNewsroomApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.sdanewsroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdaNewsroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
